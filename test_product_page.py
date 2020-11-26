7a8807215258df28bf07ec7defdb45a9a7c70b3f
ViktoriiaEvg <Viktoriiaevg@gmail.com>
2020-11-26 10:20:54 +0300

    Fixed small error

--- test_product_page.py
+++ test_product_page.py
@@ -6,7 +6,7 @@ import pytest
 import time
 
 
-@pytest.mark.parametrize('promo', ["0", "1", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
+@pytest.mark.parametrize('promo', ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
 @pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser, promo):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo}"
